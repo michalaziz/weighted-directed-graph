f37613c501881aacc0ffc19f2d45220021820b30
Michal <michalaziz1@gmail.com>
2020-01-05 13:13:35 +0200

    tsp bottun left2

--- src/dataStructure/Node.java
+++ src/dataStructure/Node.java
@@ -1,12 +1,11 @@
 package dataStructure;
 
-<<<<<<< HEAD
 import java.io.Serializable;
 
 import utils.Point3D;
 
 
-public class Node implements node_data,Serializable, Comparable<node_data>  {
+public class Node implements node_data,Serializable  {
 
 	 private int key;
 	 private double weight= Double.MAX_VALUE;
@@ -22,110 +21,25 @@ public class Node implements node_data,Serializable, Comparable<node_data>  {
 		this.location=p;
 		this.weight=weight;
 		this.tag=tag;
-		this.info=String.valueOf(key);
+		this.info=info;
 		
 	}
 	public Node(int key )
 	{
 		this.key=key;
 	}
+	/**
+	 * Default ctor
+	 */
 	public Node()
 	{
 		this.key=0;//maybe we will get node with key 0 so 0 is not the default
 		this.tag=0;
 		this.location=null;
 		this.weight=0;
-		this.info=null;
+		this.info="";
 	}
-    public Node(Node n){
-        this.key=n.key;
-        this.tag= n.tag;
-        this.weight=n.weight;
-        this.location= new Point3D(n.location);
-        this.info=n.info;
-    }
-	@Override
-	public int getKey() {
-		return this.key;
-	}
-
-	@Override
-	public Point3D getLocation() {
-		return this.location;
-	}
-
-	@Override
-	public void setLocation(Point3D p) {
-		Point3D newP= new Point3D (p);
-		this.location=newP;
-		
-	}
-
-	@Override
-	public double getWeight() {
-		return this.weight;
-	}
-
-	@Override
-	public void setWeight(double w) {
-		this.weight=w;
-	}
-
-	@Override
-	public String getInfo() {
-		return this.info;
-	}
-
-	@Override
-	public void setInfo(String s) {
-		this.info=s;
-	}
-
-	@Override
-	public int getTag() {
-		return this.tag;
-	}
-
-	@Override
-	public void setTag(int t) {
-		this.tag=t;
-
-	}
-	@Override
-	public int compareTo(node_data o) {
-		// TODO Auto-generated method stub
-		return 0;
-=======
-import utils.Point3D;
-
-
-public class Node implements node_data {
-
-	 private int key;
-	 private double weight= Double.MAX_VALUE;
-	 private int tag;
-	 private Point3D location;
-	 private String info;
-	//HashMap<Integer,Edge>fromNode= new HashMap<Integer,Edge>();
-
 	
-	public Node( int k,double weight,int tag, Point3D p, String info )
-	{
-		this.key=k;
-		this.location=p;
-		this.weight=weight;
-		this.tag=tag;
-		this.info=String.valueOf(key);
-		
-	}
-	public Node()
-	{
-		this.key=0;//maybe we will get node with key 0 so 0 is not the default
-		this.tag=0;
-		this.location=null;
-		this.weight=0;
-		this.info=null;
-	}
     public Node(Node n){
         this.key=n.key;
         this.tag= n.tag;
@@ -133,6 +47,13 @@ public class Node implements node_data {
         this.location= new Point3D(n.location);
         this.info=n.info;
     }
+    
+    public Node(int key, int weight, Point3D point) {
+		this.key = key;
+		this.weight = weight;
+		this.location = point;
+	}
+    
 	@Override
 	public int getKey() {
 		return this.key;
@@ -179,6 +100,9 @@ public class Node implements node_data {
 	public void setTag(int t) {
 		this.tag=t;
 
->>>>>>> branch 'master' of https://github.com/michalaziz/oop-Ex2.git
+	}
+	//convert the node's key to string
+	public String keytoString() {
+		return ""+this.getKey();
 	}
 }
