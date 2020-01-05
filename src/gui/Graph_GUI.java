24b368456d30781746c7abfc64401120e90adb89
michalaziz <48277596+michalaziz@users.noreply.github.com>
2020-01-05 22:06:44 +0200

    done

--- src/gui/Graph_GUI.java
+++ src/gui/Graph_GUI.java
@@ -25,8 +25,8 @@ import dataStructure.graph;
 import utils.Point3D;
 import utils.StdDraw;
 
-public class Graph_GUI extends JFrame implements ActionListener, MouseListener,Serializable {
-<<<<<<< HEAD
+public class Graph_GUI extends JFrame implements Serializable {
+
 	private graph graph ;
 	private Graph_Algo gAlgo;
 
@@ -44,6 +44,7 @@ public class Graph_GUI extends JFrame implements ActionListener, MouseListener,S
 		gAlgo=new Graph_Algo();
 		gAlgo.init(this.graph);
 		StdDraw.setGui(this);
+		initGUI();
 
 	}
 
@@ -144,420 +145,43 @@ public class Graph_GUI extends JFrame implements ActionListener, MouseListener,S
 			}
 		}
 	}
-	public static void main (String ags[])
-	{
-		//Graph_GUI gui = new Graph_GUI();
-		//gui.setVisible(true);
+	public static void main(String args[]) {
 
+		DGraph dg = new DGraph();
 
-		//		Point3D x = new Point3D(0,30,0);
-		//		Point3D y = new Point3D(-30,-15,0);
-		//		Point3D z = new Point3D(30,-15,0);
-		//		Point3D w = new Point3D(0,-20,0);
-		//		Point3D q = new Point3D(0,15,0);
-		//
-		//		node_data a = new Node(1,3,2,x,"michal");
-		//		node_data b = new Node(2,4,3,y,"yarden");
-		//		node_data c = new Node(3,5,4,z,"sf");
-		//		node_data e = new Node(4,6,5,w,"sd");
-		//		//node_data f = new Node(5,7,6,q,"ssss");
-		//
-		//		DGraph d = new DGraph();
-		//		d.addNode(a);
-		//		d.addNode(b);
-		//		d.addNode(c);
-		//		d.addNode(e);
-		//		//		d.addNode(f);
-		//		d.connect(a.getKey(),b.getKey(),1);
-		//		d.connect(b.getKey(),c.getKey(),2);
-		//		d.connect(c.getKey(),a.getKey(),3);
-		//		d.connect(c.getKey(),e.getKey(),4);
-		//		d.connect(e.getKey(),b.getKey(),4);
-		//		d.connect(e.getKey(),a.getKey(),4);
+		Point3D p1 = new Point3D(0,0,0);
+		Point3D p2 = new Point3D(0,-30,0);
+		Point3D p3 = new Point3D(40,10,0);
+		Point3D p4 = new Point3D(80,0,0);
+		Point3D p5 = new Point3D(-40,10,0);
+		Point3D p6 = new Point3D(-80,0,0);
 
-		//		Graph_GUI gui = new Graph_GUI(d);
-
-		DGraph d_g = new DGraph();
-		Graph_Algo g_a = new Graph_Algo();
-		Point3D x = new Point3D(0, 40, 3);
-		Point3D y = new Point3D(-20, 0, 3);
-		Point3D z = new Point3D(20, 0, 3);
-
-
-
-		node_data a = new Node(1, 4, 0, x, "michal");
-		node_data b = new Node(2, 4, 0, y, "michal");
-		node_data c = new Node(3, 4, 0, z, "michal");
-
-
-		d_g.addNode(a);
-		d_g.addNode(b);
-		d_g.addNode(c);
-
-		d_g.connect(1, 2, 4);
-		d_g.connect(2, 3, 4);
-		d_g.connect(3, 1, 4);
-
-
-		//d_g.removeNode(3);
-		g_a.init(d_g); 
-		Graph_GUI gui = new Graph_GUI(d_g);
-		gui.initGUI();
-
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-	}
-=======
-<<<<<<< HEAD
-	private graph graph_gui ;
-	private Graph_Algo gAlgo;
-	//LinkedList<Point3D> points = new LinkedList<Point3D>();
-
-	public Graph_Algo get_gAlgo()
-	{
-		return gAlgo;
-	}
-	public graph get_graph_gui()
-	{
-		return graph_gui;
-	}
-	public Graph_GUI(graph g)
-	{
-		this.graph_gui=g;
-		this.initGUI();
-
-	}
-
-	public Graph_GUI()
-	{
-		initGUI();
-	}
-
-	private void initGUI() 
-	{	
-/*****************************/
-	//draw the graph//
-		this.drawPoints();
-		this.drawEdges();
-	}
-
-
-
-	public void drawPoints()
-	{
-
-		StdDraw.setXscale(50, -50);
-		StdDraw.setYscale(50,-50);
-
-		Iterator<node_data> iter=  this.graph_gui.getV().iterator();
-		while(iter.hasNext())
-		{
-			node_data temp= iter.next();
-			StdDraw.setPenRadius(0.02);
-			StdDraw.setPenColor(StdDraw.BLACK);
-			StdDraw.point(temp.getLocation().x(),temp.getLocation().y());
-			StdDraw.setPenColor(StdDraw.RED);
-			StdDraw.setPenRadius(0.001);
-			StdDraw.text(temp.getLocation().x()-0.1, temp.getLocation().y()-3,""+temp.getKey());;
-		}
-	}
-
-	public void drawEdges()
-	{
-		Iterator<node_data> iterN=  this.graph_gui.getV().iterator();
-		while(iterN.hasNext())
-		{
-			node_data tempV=iterN.next();
-			Collection<edge_data> edges = this.graph_gui.getE(tempV.getKey());
-			if(edges==null)
-				break;
-			Iterator<edge_data> iterE=edges.iterator();
-			while(iterE.hasNext())
-			{
-				StdDraw.setPenRadius(0.003);
-				StdDraw.setPenColor(StdDraw.BLACK);
-				edge_data tempE=iterE.next();
-				StdDraw.line(graph_gui.getNode(tempE.getSrc()).getLocation().x(),
-						graph_gui.getNode(tempE.getSrc()).getLocation().y(),
-						graph_gui.getNode(tempE.getDest()).getLocation().x(),
-						graph_gui.getNode(tempE.getDest()).getLocation().y());
-				StdDraw.setPenRadius(0.015);
-				StdDraw.setPenColor(StdDraw.GREEN);
-				StdDraw.point(((graph_gui.getNode(tempE.getSrc()).getLocation().x()+
-						graph_gui.getNode(tempE.getDest()).getLocation().x()*7)/8),
-						((graph_gui.getNode(tempE.getSrc()).getLocation().y())+
-								graph_gui.getNode(tempE.getDest()).getLocation().y()*7)/8);
-				StdDraw.setPenColor(StdDraw.BOOK_BLUE);
-				StdDraw.text(((graph_gui.getNode(tempE.getSrc()).getLocation().x()+
-						graph_gui.getNode(tempE.getDest()).getLocation().x())/2),
-						((graph_gui.getNode(tempE.getSrc()).getLocation().y())+
-								graph_gui.getNode(tempE.getDest()).getLocation().y())/2,
-						""+tempE.getWeight());
-			}
-		}
-	}
-	public static void main (String ags[])
-	{
-		//Graph_GUI gui = new Graph_GUI();
-		//gui.setVisible(true);
-
-
-		Point3D x = new Point3D(0,30,0);
-		Point3D y = new Point3D(-30,-15,0);
-		Point3D z = new Point3D(30,-15,0);
-		Point3D w = new Point3D(0,-20,0);
-		Point3D q = new Point3D(0,15,0);
-
-		node_data a = new Node(1,3,2,x,"michal");
-		node_data b = new Node(2,4,3,y,"yarden");
-		node_data c = new Node(3,5,4,z,"sf");
-		node_data e = new Node(4,6,5,w,"sd");
-		//node_data f = new Node(5,7,6,q,"ssss");
-
-		DGraph d = new DGraph();
-		d.addNode(a);
-		d.addNode(b);
-		d.addNode(c);
-		d.addNode(e);
-		//		d.addNode(f);
-		d.connect(a.getKey(),b.getKey(),1);
-		d.connect(b.getKey(),c.getKey(),2);
-		d.connect(c.getKey(),a.getKey(),3);
-		d.connect(c.getKey(),e.getKey(),4);
-		d.connect(e.getKey(),b.getKey(),4);
-		d.connect(e.getKey(),a.getKey(),4);
-		Graph_GUI gui = new Graph_GUI(d);
-
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		Node n1 = new Node(1,1,1,p1,"1");
+		Node n2 = new Node(2,2,2,p2,"2");
+		Node n3 = new Node(3,3,3,p3,"3");
+		Node n4 = new Node(4,4,4,p4,"4");
+		Node n5 = new Node(5,5,5,p5,"5");
+		Node n6 = new Node(6,6,6,p6,"16");
+		
+		dg.addNode(n1);
+		dg.addNode(n2);
+		dg.addNode(n3);
+		dg.addNode(n4);
+		dg.addNode(n5);
+		dg.addNode(n6);
+		
+		dg.connect(1, 5, 1);
+		dg.connect(5, 6, 2);
+		dg.connect(6, 2, 3);
+		dg.connect(2, 4, 4);
+		dg.connect(4, 3, 5);
+		dg.connect(3, 1, 6);
+		
+		Graph_GUI gui = new Graph_GUI(dg);
+		
 		
 	}
 
-}
-=======
-    private graph graph_gui ;
-    private Graph_Algo gAlgo;
-    //LinkedList<Point3D> points = new LinkedList<Point3D>();
->>>>>>> d2cdcb04b6db5d5e92d7f82b9375f3ccad7512e5
 
-    public Graph_Algo get_gAlgo()
-    {
-        return gAlgo;
-    }
-    public graph get_graph_gui()
-    {
-        return graph_gui;
-    }
-    public Graph_GUI(graph g)
-    {
-        this.graph_gui=g;
-        this.initGUI();
-
-    }
-
-    public Graph_GUI()
-    {
-        initGUI();
-    }
-
-    private void initGUI()
-    {
-/***********/
-        //draw the graph//
-        this.drawPoints();
-        this.drawEdges();
-    }
-
-
-
-    public void drawPoints()
-    {
-
-        StdDraw.setXscale(50, -50);
-        StdDraw.setYscale(50,-50);
-
-        Iterator<node_data> iter=  this.graph_gui.getV().iterator();
-        while(iter.hasNext())
-        {
-            node_data temp= iter.next();
-            StdDraw.setPenRadius(0.02);
-            StdDraw.setPenColor(StdDraw.BLACK);
-            StdDraw.point(temp.getLocation().x(),temp.getLocation().y());
-            StdDraw.setPenColor(StdDraw.RED);
-            StdDraw.setPenRadius(0.001);
-            StdDraw.text(temp.getLocation().x()-0.1, temp.getLocation().y()-3,""+temp.getKey());;
-        }
-    }
-
-    public void drawEdges()
-    {
-        Iterator<node_data> iterN=  this.graph_gui.getV().iterator();
-        while(iterN.hasNext())
-        {
-            node_data tempV=iterN.next();
-            Collection<edge_data> edges = this.graph_gui.getE(tempV.getKey());
-            if(edges==null)
-                break;
-            Iterator<edge_data> iterE = edges.iterator();
-            while(iterE.hasNext())
-            {
-                StdDraw.setPenRadius(0.003);
-                StdDraw.setPenColor(StdDraw.BLACK);
-                edge_data tempE=iterE.next();
-                StdDraw.line(graph_gui.getNode(tempE.getSrc()).getLocation().x(),
-                        graph_gui.getNode(tempE.getSrc()).getLocation().y(),
-                        graph_gui.getNode(tempE.getDest()).getLocation().x(),
-                        graph_gui.getNode(tempE.getDest()).getLocation().y());
-                StdDraw.setPenRadius(0.015);
-                StdDraw.setPenColor(StdDraw.GREEN);
-                StdDraw.point(((graph_gui.getNode(tempE.getSrc()).getLocation().x()+
-                                graph_gui.getNode(tempE.getDest()).getLocation().x()*7)/8),
-                        ((graph_gui.getNode(tempE.getSrc()).getLocation().y())+
-                                graph_gui.getNode(tempE.getDest()).getLocation().y()*7)/8);
-                StdDraw.setPenColor(StdDraw.BOOK_BLUE);
-                StdDraw.text(((graph_gui.getNode(tempE.getSrc()).getLocation().x()+
-                                graph_gui.getNode(tempE.getDest()).getLocation().x())/2),
-                        ((graph_gui.getNode(tempE.getSrc()).getLocation().y())+
-                                graph_gui.getNode(tempE.getDest()).getLocation().y())/2,
-                        ""+tempE.getWeight());
-            }
-        }
-    }
-    public static void main (String ags[])
-    {
-        //Graph_GUI gui = new Graph_GUI();
-        //gui.setVisible(true);
-
-
-        Point3D x = new Point3D(0,30,0);
-        Point3D y = new Point3D(-30,-15,0);
-        Point3D z = new Point3D(30,-15,0);
-        Point3D w = new Point3D(0,-20,0);
-        Point3D q = new Point3D(0,15,0);
-
-        node_data a = new Node(1,3,2,x,"michal");
-        node_data b = new Node(2,4,3,y,"yarden");
-        node_data c = new Node(3,5,4,z,"sf");
-        node_data e = new Node(4,6,5,w,"sd");
-        //node_data f = new Node(5,7,6,q,"ssss");
-
-        DGraph d = new DGraph();
-        d.addNode(a);
-        d.addNode(b);
-        d.addNode(c);
-        d.addNode(e);
-        //		d.addNode(f);
-        d.connect(a.getKey(),b.getKey(),1);
-        d.connect(b.getKey(),c.getKey(),2);
-        d.connect(c.getKey(),a.getKey(),3);
-        d.connect(c.getKey(),e.getKey(),4);
-        d.connect(e.getKey(),b.getKey(),4);
-        d.connect(e.getKey(),a.getKey(),4);
-
-        Graph_GUI gui = new Graph_GUI(d);
-
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-
-    }
->>>>>>> branch 'master' of https://github.com/michalaziz/oop-Ex2.git
 
 }
