3cf2e79d40814dfe22fcf9e0360a5905c5c8b899
michalaziz <48277596+michalaziz@users.noreply.github.com>
2020-01-05 22:06:18 +0200

    done

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -12,7 +12,9 @@ public class DGraph implements graph,Serializable {
 
 	HashMap<Integer,node_data> nodes = new HashMap<>();
 	HashMap<Integer,HashMap<Integer,edge_data>> edges = new HashMap<>();	
-	int nodeSize=0,edgeSize=0,MC=0;
+	int nodeSize=0;
+	int edgeSize=0;
+	int MC=0;
 
 	/**
 	 * constructor
@@ -158,14 +160,16 @@ public class DGraph implements graph,Serializable {
 	 */
 	@Override
 	public edge_data removeEdge(int src, int dest) {
-		edge_data remove_edge=edges.get(src).remove(dest);
-		if(remove_edge!=null) {
-			MC++;
-			edgeSize--;
+		edge_data temp =this.getEdge(src,dest);
+		if(temp!=null) {
+			this.edges.get(src).remove(dest);
+			this.edgeSize--;
+			this.MC++;
+			return temp ;
 		}
-		return remove_edge;
-
+		return temp;
 	}
+
 
 	/**
 	 * @return the number of vertices in the graph.
@@ -189,53 +193,6 @@ public class DGraph implements graph,Serializable {
 	@Override
 	public int getMC() {
 		return this.MC;
-	}
-
-	public static void main(String args[])
-	{
-		//		Point3D x1 = new Point3D(14,4,0);
-		//		Point3D y2 = new Point3D(-75,14,0);
-		//		Point3D q3 = new Point3D(80,5,0);
-		//		
-		//		node_data a1 = new Node(4,2,3,x,"asf");
-		//		node_data b2 = new Node(5,4,6,y,"ads");
-		//		node_data c3 = new Node(6,50,50,q,"sf");
-		//		
-
-
-
-
-		Point3D x = new Point3D(1,2,3);
-		Point3D y = new Point3D(-1,-2,-3);
-		Point3D z = new Point3D(6,7,8);
-
-		node_data a = new Node(1,3,2,x,"michal");
-		node_data b = new Node(2,4,3,y,"yarden");
-		node_data c = new Node(3,5,4,z,"sf");
-
-		DGraph d = new DGraph();
-		d.addNode(a);
-		d.addNode(b);
-		d.addNode(c);
-		d.connect(a.getKey(),b.getKey(),10);
-		d.connect(b.getKey(),c.getKey(),10);
-		//d.connect(c.getKey(),a.getKey(),10);
-		//		d.connect(b.getKey(),a.getKey(),13);
-		//System.out.println(d.Edgemap.toString());
-
-		//		d.addNode(a1);
-		//		d.addNode(b2);
-		//		d.addNode(c3);
-		//		d.connect(a.getKey(),b.getKey(),2);
-		//		d.connect(a.getKey(),a1.getKey(),4);
-		//		d.connect(a.getKey(),c.getKey(),1);
-		//		d.connect(c.getKey() ,b2.getKey(),1);
-		//		d.connect(b2.getKey(),a1.getKey(),1);
-		//		d.connect(a1.getKey(),c3.getKey(),1);
-		//		d.connect(b.getKey(),c3.getKey(),1);
-
-		Graph_Algo p = new Graph_Algo(d);
-		System.out.println(p.isConnected());
 	}
 
 }
--- src/dataStructure/Edge.java
+++ src/dataStructure/Edge.java
@@ -1,12 +1,9 @@
 package dataStructure;
 
-<<<<<<< HEAD
 import java.io.Serializable;
 
 public class Edge implements edge_data,Serializable {
-=======
-public class Edge implements edge_data{
->>>>>>> branch 'master' of https://github.com/michalaziz/oop-Ex2.git
+
 
 	private int src; 
 	private int dest;
